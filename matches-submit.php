d17c7b0770f2fd72c4612420e4b9169bb210e0fe
edverma <edverma@icloud.com>
2019-04-02 19:55:53 -0400

    Ok all form validation works now except minmax age difference

--- matches-submit.php
+++ matches-submit.php
@@ -18,8 +18,8 @@
 
 	<?php
 	
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  			if (empty($_POST["name"])) {
+		if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  			if (empty($_GET["name"])) {
     			exit("Name is required.");
   			} 
   		}
